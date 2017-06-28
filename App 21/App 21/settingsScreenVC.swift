cc31e72846619185ae05b77449304e34145ae532
Abdullah Şahin <abdullahsahinceo@gmail.com>
2017-06-28 16:57:32 +0300

    Güncelleme
    1) settingsScreenVC güncellendi.
    2) Gereksiz assetsler silindi.
    3) feedbackScreenVC oluşturuldu.

--- App 21/App 21/settingsScreenVC.swift
+++ App 21/App 21/settingsScreenVC.swift
@@ -34,7 +34,12 @@ class settingsScreenVC: UIViewController {
     //MARK: backButton nesnesi oluşturuldu.
     let backButton: UIButton = {
        let button = UIButton()
-        button.setImage(#imageLiteral(resourceName: "geriBtn"), for: .normal)
+        button.backgroundColor = UIColor.white
+        button.layer.cornerRadius = 5
+        button.setTitle("Geri", for: UIControlState.normal)
+        button.setTitleColor(UIColor(red: 0.31, green: 0.36, blue: 0.43, alpha: 1.0), for: UIControlState.normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Book", size: 36)
+        button.addTarget(self, action: #selector(backMainButton), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -45,15 +50,6 @@ class settingsScreenVC: UIViewController {
         image.image = UIImage(named: "infoBG")
         image.translatesAutoresizingMaskIntoConstraints = false
         image.frame = CGRect(x: 0, y: 0, width: 317, height: 188)
-        return image
-    }()
-    
-    //MARK: languageBG nesnesi oluşturuldu.
-    let languageBg : UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "languageBG")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.frame = CGRect(x: 0, y: 0, width: 317, height: 97)
         return image
     }()
     
@@ -78,7 +74,7 @@ class settingsScreenVC: UIViewController {
     }()
     
     //MARK: versionnumberLabel nesnesi oluşturuldu.
-    let versionnumberLabel : UILabel = {
+    let versionNumberLabel : UILabel = {
        let label = UILabel()
         label.text = "0.0.1"
         label.textColor = UIColor.white
@@ -101,57 +97,46 @@ class settingsScreenVC: UIViewController {
         return label
     }()
     
-    //MARK: languageLabel nesnesi oluşturuldu.
-    let languageLabel : UILabel = {
-       let label = UILabel()
-        label.text = "Dil Seçenekleri"
-        label.textColor = UIColor.white
-        label.font = UIFont(name: "Avenir-Book", size: 10)
-        label.numberOfLines = 1
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
     //MARK: feedbackButton nesnesi oluşturuldu.
-    let feedbackButton : UIButton = {
+    let istekButton : UIButton = {
        let button = UIButton()
-        button.setImage(#imageLiteral(resourceName: "feedbackButtonBG"), for: .normal)
+        button.backgroundColor = UIColor(red: 0.31, green: 0.36, blue: 0.43, alpha: 1.0)
+        button.layer.cornerRadius = 5
+        button.setTitle("İstek ve Görüşleriniz", for: UIControlState.normal)
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Book", size: 20)
+        button.addTarget(self, action: #selector(feedbackButton), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
-    }()
-    
-    //MARK: feedbackLabel nesnesi oluşturuldu.
-    let feedbackLabel : UILabel = {
-        let label = UILabel()
-        label.text = "İstek ve Görüşleriniz"
-        label.textColor = UIColor.white
-        label.font = UIFont(name: "Avenir-Book", size: 20)
-        label.numberOfLines = 1
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
     }()
     
     //MARK: rateButton nesnesi oluşturuldu.
-    let rateButton : UIButton = {
+    let oyButton : UIButton = {
       let button = UIButton()
-        button.setImage(#imageLiteral(resourceName: "rateButtonBG"), for: .normal)
+        button.backgroundColor = UIColor(red: 0.31, green: 0.36, blue: 0.43, alpha: 1.0)
+        button.layer.cornerRadius = 5
+        button.setTitle("App Store'da Bize Destek Ol", for: UIControlState.normal)
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Book", size: 20)
+        button.addTarget(self, action: #selector(rateButton), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
-    //MARK: rateLabel nesnesi oluşturuldu.
-    let rateLabel : UILabel = {
-       let label = UILabel()
-        label.text = "App Store'da Oy Ver"
-        label.textColor = UIColor.white
-        label.font = UIFont(name: "Avenir-Book", size: 20)
-        label.numberOfLines = 1
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    //MARK: startButton fonksiyonu. Oyunu başlatıyor.
+    func backMainButton(sender: UIButton!){
+        self.navigationController?.pushViewController(mainScreenVC(), animated: true)
+    }
+    
+    //MARK: feedbackButton fonksiyonu. Geri bildirim ekranına yönlendiriyor.
+    func feedbackButton(sender: UIButton!){
+        self.navigationController?.pushViewController(feedbackScreenVC(), animated: true)
+    }
+    
+    //MARK: feedbackButton fonksiyonu. Geri bildirim ekranına yönlendiriyor.
+    func rateButton(sender: UIButton!){
+        self.navigationController?.pushViewController(settingsScreenVC(), animated: true)
+    }
     
     //MARK: Nesneler ekrana ekleniyor ve constraint değerleri belirleniyor.
     func setupviews(){
@@ -160,39 +145,43 @@ class settingsScreenVC: UIViewController {
         view.addSubview(infoBg)
         view.addSubview(appLogo)
         view.addSubview(versionLabel)
-        view.addSubview(versionnumberLabel)
+        view.addSubview(versionNumberLabel)
         view.addSubview(madeLabel)
-        view.addSubview(languageBg)
-        view.addSubview(languageLabel)
-        view.addSubview(feedbackButton)
-        view.addSubview(feedbackLabel)
-        view.addSubview(rateButton)
+        view.addSubview(istekButton)
+        view.addSubview(oyButton)
         
         //MARK: Nesnelerin constraint değerleri belirleniyor.
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-15-[backButton]", options: NSLayoutFormatOptions(), metrics: nil, views: ["backButton" : backButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[infoBG]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["infoBG" : infoBg]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-160-[appLogo(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["appLogo" : appLogo]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-160-[versionLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["versionLabel" : versionLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-170-[versionNumberLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["versionNumberLabel" : versionnumberLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-125-[madeLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["madeLabel" : madeLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[languageBg]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["languageBg" : languageBg]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-25-[languageLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["languageLabel" : languageLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[feedbackButton]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["feedbackButton" : feedbackButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-125-[feedbackLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["feedbackLabel" : feedbackLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[rateButton]-10-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["rateButton" : rateButton]))
+        let backButtonTop = NSLayoutConstraint(item: backButton, attribute:.top, relatedBy: .equal, toItem: self.view, attribute:.top, multiplier: 1.0, constant: 19)
+        let backButtonLeading = NSLayoutConstraint(item: backButton, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 30)
         
-        //MARK: Nesnelerin birbiri ile olan constraint değerleri belirleniyıor.
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-25-[backButton]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "backButton" : backButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[backButton]-15-[infoBG]", options: NSLayoutFormatOptions(), metrics: nil, views: ["backButton" : backButton, "infoBG" : infoBg]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-70-[appLogo]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "appLogo" : appLogo]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-125-[versionLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "versionLabel" : versionLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-145-[versionNumberLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "versionNumberLabel" : versionnumberLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-220-[madeLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "madeLabel" : madeLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-265-[languageBg]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "languageBg" : languageBg]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-275-[languageLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "languageLabel" : languageLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-375-[feedbackButton]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "feedbackButton" : feedbackButton]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-407-[feedbackLabel]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "feedbackLabel" : feedbackLabel]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-485-[rateButton]", options: NSLayoutFormatOptions(), metrics: nil, views: ["topGuide" : topLayoutGuide, "rateButton" : rateButton]))
+        let infoBGTop = NSLayoutConstraint(item: infoBg, attribute: .top, relatedBy: .equal, toItem: self.backButton, attribute: .top, multiplier: 1.0, constant: 60)
+        let infoBGCenter = NSLayoutConstraint(item: infoBg, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        let infoBGWidth = NSLayoutConstraint(item: infoBg, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 300)
+        
+        let appLogoTop = NSLayoutConstraint(item: appLogo, attribute: .top, relatedBy: .equal, toItem: self.infoBg, attribute: .top, multiplier: 1.0, constant: 15)
+        let appLogoCenter = NSLayoutConstraint(item: appLogo, attribute: .centerX, relatedBy: .equal, toItem: self.infoBg, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        
+        let versionLabelTop = NSLayoutConstraint(item: versionLabel, attribute: .top, relatedBy: .equal, toItem: self.appLogo, attribute: .top, multiplier: 1.0, constant: 55)
+        let versionLabelCenter = NSLayoutConstraint(item: versionLabel, attribute: .centerX, relatedBy: .equal, toItem: self.infoBg, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        
+        let versionNumberLabelTop = NSLayoutConstraint(item: versionNumberLabel, attribute: .top, relatedBy: .equal, toItem: self.versionLabel, attribute: .top, multiplier: 1.0, constant: 20)
+        let versionNumberLabelCenter = NSLayoutConstraint(item: versionNumberLabel, attribute: .centerX, relatedBy: .equal, toItem: self.infoBg, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        
+        let madeLabelTop = NSLayoutConstraint(item: madeLabel, attribute: .top, relatedBy: .equal, toItem: self.versionNumberLabel, attribute: .top, multiplier: 1.0, constant: 75)
+        let madeLabelCenter = NSLayoutConstraint(item: madeLabel, attribute: .centerX, relatedBy: .equal, toItem: self.infoBg, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        
+        let istekButtonTop = NSLayoutConstraint(item: istekButton, attribute: .top, relatedBy: .equal, toItem: self.infoBg, attribute: .top, multiplier: 1.0, constant: 200)
+        let istekButtonCenter = NSLayoutConstraint(item: istekButton, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        let istekButtonHeight = NSLayoutConstraint(item: istekButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 75)
+        let istekButtonWidth = NSLayoutConstraint(item: istekButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 297)
+        
+        let oyButtonTop = NSLayoutConstraint(item: oyButton, attribute: .top, relatedBy: .equal, toItem: self.istekButton, attribute: .top, multiplier: 1.0, constant: 85)
+        let oyButtonCenter = NSLayoutConstraint(item: oyButton, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        let oyButtonHeight = NSLayoutConstraint(item: oyButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 75)
+        let oyButtonWidth = NSLayoutConstraint(item: oyButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 297)
+        
+        //MARK: Nesnelerin constraint değerleri ekrana ekleniyor.
+        NSLayoutConstraint.activate([backButtonTop,backButtonLeading,infoBGTop,infoBGCenter,infoBGWidth,appLogoTop,appLogoCenter,versionLabelTop,versionLabelCenter,versionNumberLabelTop,versionNumberLabelCenter,madeLabelTop,madeLabelCenter,istekButtonTop,istekButtonCenter,istekButtonHeight,istekButtonWidth,oyButtonTop,oyButtonWidth,oyButtonCenter,oyButtonHeight])
     }
 }
 
---
+++ App 21/feedbackScreenVC.swift
@@ -0,0 +1,16 @@
+//
+//  feedbackScreenVC.swift
+//  App 21
+//
+//  Created by Abdullah on 28.06.2017.
+//  Copyright © 2017 kodamanlabs. All rights reserved.
+//
+
+import UIKit
+
+class feedbackScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
