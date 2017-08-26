b2e16cbcf62300d07a522c03df8b11d423bf25bc
Abdullah Şahin <abdullahsahinceo@gmail.com>
2017-08-26 07:27:15 +0300

    Güncelleme
    - Eski proje bazı hataların tespit edilememesinden dolayı silindi.
    - PureLayout entegre edildi.

---
+++ App 21/Number 21/Number 21/mainScreenVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Number 21
+//
+//  Created by Abdullah on 26.08.2017.
+//  Copyright © 2017 abdullah. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+class mainScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
